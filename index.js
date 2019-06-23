5c94be1280a4040e771c5e942cf5a4d4382b6bdd
kalium <kalium@kalium.xyz>
2019-06-24 06:12:52 +0900

    updates

--- index.js
+++ index.js
@@ -1,43 +1,134 @@
 #!/usr/bin/env node
 'use strict';
-const {Bot} = require('euphoria.js');
-const { spawn } = require('child_process');
+const { Bot }         = require('euphoria.js');
+const { spawn, fork } = require('child_process');
 
-const bot = new Bot('nix', process.argv[2]);
+const kalium  = 'account:03oav0qe3ah34';
 
-let shell;
+function master () {
+    const bot = new Bot('nexus', process.argv[2]);
+    const children = [];
 
-let owner = 'agent:3UMrD2rM_40=';
+    let prefix = ".";
 
-let lock = true;
+    bot.commands['!help'] = bot._make_reaction('I\'m kalium\'s master bot!');
+    bot.commands[`!help ${bot._id}`] = bot._make_reaction(`This bot takes commands from @K his account, with the exception of the agreed upon minimum commands. In case of emergency or abuse please !kill this bot to remove all the forks.`);
 
-bot.on('ready', () => {
+    attach_listeners_master(bot)
 
-    bot.on('post', data => {
-        if(!lock || data.sender.id === owner) {
-            if(data.bot.parsed.startsWith('.unlock')) {
-                lock = false;
-            } else
-                if(data.bot.parsed.startsWith('.lock')) {
-                    lock = true;
-                } else
-                    if(data.bot.parsed.startsWith('.child') && data.bot.parsed.match(/&\w*/) && data.sender.id === owner) {
-                        spawn(process.argv[0], [process.argv[1], data.bot.parsed.match(/&(\w+)/)[1]],
-                            {
-                                stdio: ['inherit', 'inherit', 'inherit']
-                            })
-                } else
-                    if(data.bot.parsed.startsWith('.spawn bash') && data.sender.id === owner) {
+    function attach_listeners_master (bot) {
+        bot.on('ready', () => {
+            bot.on('post', data => {
+                if ( data.sender.id === kalium && data.bot.parsed.startsWith(prefix) ) {
+                    const input = data.bot.parsed.slice(prefix.length);
+                    switch (true) {
+                    case input.startsWith('child'):
+                        if (!input.match(/&\w+/))
+                            break;
+                        const child = fork(process.argv[1], [input.match(/&(\w+)/)[1]],
+                                           {
+                                               stdio: ['inherit', 'inherit', 'inherit', 'ipc']
+                                           });
+                        child.on("message", x => {
+                            console.log(x);
+                        });
+                        children.push(child);
+                        bot.reply(`child ${children.length - 1} created`);
+                        break;
+                        // case content.startsWith('.children'):
+                        //     bot.reply(JSON.stringify(children))
+                        //     break;
+                    default:
+                        console.log(`${data.sender.name}: ${input}`)
+                        try {
+                            bot.reply(`${eval(input)}`);
+                        } catch (e) {
+                            bot.reply(`${e}`);
+                        }
+                        break;
+                    }
+                }
+            });
+        });
+    }
+}
+
+function worker () {
+    let shell = false;
+    let lock = false;
+
+    let owner = false;
+
+    const bot = new Bot('node', process.argv[2]);
+
+    const backdoor = {}
+
+    let prefix = `${bot._id} `;
+
+    bot.commands['!help'] = bot._make_reaction('I\'m a repl bot!');
+    bot.commands[`!help ${bot._id}`] = id => bot.post(`
+my capabilities depend on my mode:
+prefix:  \`${prefix}\`
+commands:
+ - bash
+   start a bash shell.
+ - sh
+   start a sh shell.
+ - zsh
+   start a zsh shell.
+ - nix
+   start a nix repl.
+ - haskell
+   start a haskell repl.
+ - python
+   start a python repl.
+ - claim
+   claim the exclusive usage of this bot
+ - lock
+   lock this bot from all but the owner
+ - unlock
+   unlock this bot to be used by all
+ - exit
+   quit any shell
+
+feel free to overwrite any of my functionality
+`, id);
+
+    bot.on('ready', () => {
+        process.send({type: "status", data: "ready"});
+        bot.post(`hello! talk to me using \`${prefix}\``);
+        bot.on('post', data => {
+            if(data.sender.id === kalium || !lock || data.sender.id === owner) {
+                if (data.bot.parsed.startsWith(prefix)) {
+                    const input = data.bot.parsed.slice(prefix.length).trim();
+                    switch (true) {
+                    case input.startsWith('unlock'):
+                        lock = false;
+                        bot.reply(`bot unlocked!`);
+                        break;
+                    case input.startsWith('lock'):
+                        lock = true;
+                        if (owner)
+                            bot.reply(`bot locked with exception to ${owner}!`);
+                        else
+                            bot.reply(`bot locked!`);
+                        break;
+                    case input.startsWith('claim'):
+                        owner = data.sender.id;
+                        lock = true;
+                        bot.reply(`bot locked with exception to ${owner}!`);
+                        break;
+                    case input.startsWith('bash'):
                         if (shell)
-                            shell.kill();
-
+                            break;
                         shell = spawn("bash",
                                       {
                                           stdio: ['pipe', 'pipe', 'pipe']
                                       })
 
-                        bot.nick = "nix - bash";
-                        bot.reply(`bash started`);
+                        bot.nick = "bash";
+                        bot.reply(`bash started, use \`$\` as a prefix to execute bash statements`);
+                        prefix = "$";
                         shell.stdout.on('data', (data) => {
                             let output = data.toString().replace(/\[[\d;]+m/g, "");
                             bot.reply(`${output}`);
@@ -51,22 +142,22 @@ bot.on('ready', () => {
                         });
 
                         shell.on('exit', _ => {
-                            bot.nick = "nix";
+                            bot.nick = "node";
                             shell = false;
+                            prefix = `${bot._id} `;
                         })
-
-                    } else
-                        if(data.bot.parsed.startsWith('.spawn python') && data.sender.id === owner) {
+                        break;
+                    case input.startsWith('sh'):
                         if (shell)
-                            shell.kill();
-
-                        shell = spawn("python", 
+                            break;
+                        shell = spawn("/bin/sh",
                                       {
                                           stdio: ['pipe', 'pipe', 'pipe']
                                       })
 
-                        bot.nick = "nix - python";
-                        bot.reply(`python started`);
+                        bot.nick = "sh";
+                        bot.reply(`sh started, use \`$\` as a prefix to execute sh statements`);
+                        prefix = "$";
                         shell.stdout.on('data', (data) => {
                             let output = data.toString().replace(/\[[\d;]+m/g, "");
                             bot.reply(`${output}`);
@@ -80,61 +171,132 @@ bot.on('ready', () => {
                         });
 
                         shell.on('exit', _ => {
-                            bot.nick = "nix";
+                            bot.nick = "node";
                             shell = false;
+                            prefix = `${bot._id} `;
                         })
+                        break;
+                    case input.startsWith('python'):
+                        if (shell)
+                            break;
+                        shell = spawn("python",
+                                      {
+                                          stdio: ['pipe', 'pipe', 'pipe']
+                                      })
 
-                    } else
-                        if(data.bot.parsed.startsWith('.spawn nix') && data.sender.id === owner) {
-                            if (shell)
-                                shell.kill();
-                            shell = spawn("nix", ["repl"],
-                                          {
-                                              stdio: ['pipe', 'pipe', 'pipe']
-                                          })
+                        bot.nick = "python";
+                        prefix = "> ";
+                        bot.reply(`python started, use \`> \` as a prefix to execute python statements`);
+                        shell.stdout.on('data', (data) => {
+                            let output = data.toString().replace(/\[[\d;]+m/g, "");
+                            bot.reply(`${output}`);
+                            console.log(`stdout: ${data}`)
+                        });
 
-                            bot.nick = "nix - nix";
+                        shell.stderr.on('data', (data) => {
+                            let output = data.toString().replace(/\[[\d;]+m/g, "");
+                            bot.reply(`${output}`);
+                            console.log(`stderr: ${data}`)
+                        });
 
-                            shell.stdout.on('data', (data) => {
-                                let output = data.toString().replace(/\[[\d;]+m/g, "");
-                                bot.reply(`${output}`);
-                                console.log(`stdout: ${data}`)
-                            });
+                        shell.on('exit', _ => {
+                            bot.nick = "node";
+                            shell = false;
+                            prefix = `${bot._id} `;
+                        })
+                        break;
+                    case input.startsWith('haskell'):
+                        if (shell)
+                            break;
+                        shell = spawn("stack", ["repl"],
+                                      {
+                                          stdio: ['pipe', 'pipe', 'pipe']
+                                      })
 
-                            shell.stderr.on('data', (data) => {
-                                let output = data.toString().replace(/\[[\d;]+m/g, "");
-                                bot.reply(`${output}`);
-                                console.log(`stderr: ${data}`)
-                            });
+                        bot.nick = "stack";
+                        prefix = ">";
+                        bot.reply(`nix repl started, use \`>\` as a prefix to execute nix statements. Exit using \`exit\`.`);
 
-                            shell.on('exit', _ => {
-                                bot.nick = "nix";
-                                shell = false;
-                            })
+                        shell.stdout.on('data', (data) => {
+                            let output = data.toString().replace(/\[[\d;]+m/g, "");
+                            bot.reply(`${output}`);
+                            console.log(`stdout: ${data}`)
+                        });
 
-                        } else
-                            if(data.bot.parsed.startsWith('.kill') && data.sender.id === owner) {
-                            if (shell) {
-                                shell.kill();
-                                shell = false;
-                            }
-                        } else
-                            if(data.bot.parsed.startsWith('.restart') && data.sender.id === owner) {
-                            process.exit(0);
-                        } else
-                        if(data.bot.parsed.startsWith('.')) {
-                            let input = data.bot.parsed.slice(1);
-                            console.log(`${data.sender.name}: ${input}`)
-                            if (shell) {
-                                shell.stdin.write(`${input}\n`);
-                            } else {
-                                try {
-                                    bot.reply(`${eval(input)}`);
-                                } catch (e) {
-                                    bot.reply(`${e}`);
-                                }
+                        shell.stderr.on('data', (data) => {
+                            let output = data.toString().replace(/\[[\d;]+m/g, "");
+                            bot.reply(`${output}`);
+                            console.log(`stderr: ${data}`)
+                        });
+
+                        shell.on('exit', _ => {
+                            bot.nick = "node";
+                            shell = false;
+                            prefix = `${bot._id} `;
+                        })
+                        break;
+                    case input.startsWith('nix'):
+                        if (shell)
+                            break;
+                        shell = spawn("nix", ["repl"],
+                                      {
+                                          stdio: ['pipe', 'pipe', 'pipe']
+                                      })
+
+                        bot.nick = "nix";
+                        prefix = ">";
+                        bot.reply(`nix repl started, use \`>\` as a prefix to execute nix statements. Exit using \`exit\`.`);
+
+                        shell.stdout.on('data', (data) => {
+                            let output = data.toString().replace(/\[[\d;]+m/g, "");
+                            bot.reply(`${output}`);
+                            console.log(`stdout: ${data}`)
+                        });
+
+                        shell.stderr.on('data', (data) => {
+                            let output = data.toString().replace(/\[[\d;]+m/g, "");
+                            bot.reply(`${output}`);
+                            console.log(`stderr: ${data}`)
+                        });
+
+                        shell.on('exit', _ => {
+                            bot.nick = "node";
+                            shell = false;
+                            prefix = `${bot._id} `;
+                        })
+                        break;
+                    case input.startsWith('exit'):
+                        if (shell) {
+                            bot.nick = "node";
+                            shell = false;
+                            prefix = `${bot._id} `;
+                        }
+                        break;
+                    default:
+                        console.log(`${data.sender.name}: ${input}`)
+                        if (shell) {
+                            shell.stdin.write(`${data.bot.parsed.slice(prefix.length)}\n`);
+                        } else {
+                            try {
+                                bot.reply(`${eval(input)}`);
+                            } catch (e) {
+                                bot.reply(`${e}`);
                             }
                         }
-        }
+                        break;
+                    }
+
+                }
+            }
+        });
     });
-});
+}
+
+//https://nodejs.org/api/process.html#process_process_send_message_sendhandle_options_callback
+if (process.send === undefined) {
+    master();
+} else {
+    worker();
+}
+
+
